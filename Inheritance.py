8a1094f11f461c70229867cad5da8b36b205265e
9i6i <44381250+9i6i@users.noreply.github.com>
2019-02-28 14:33:31 -0800

    Inheritance notes

---
+++ Inheritance.py
@@ -0,0 +1,39 @@
+class Vehicle(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Car(Vehicle):
+    def __init__(self, name):
+        super(Car, self).__init__(name)
+        self.engine_status = False
+        self.fuel = 100
+
+    def start_engine(self):
+        self.engine_status = True
+        print("You turn the key and the engine starts.")
+
+    def move_forward(self):
+        self.fuel -= 1
+        print("You move forward.")
+
+    def turn_left(self):
+        self.fuel -= 1
+        print("SHE'S MAKING A LEFT TURN!!!!")
+
+    def turn_orr(self):
+        self.engine_status = False
+        print("You turn the engine off.")
+
+
+class Viper(Car):
+    def __init__(self):
+        super(Viper, self).__init__("Viper")
+
+
+brisa_car = Viper()
+brisa_car.start_engine()
+brisa_car.move_forward()
+brisa_car.turn_left()
+brisa_car.move_forward()
+brisa_car.turn_orr()
