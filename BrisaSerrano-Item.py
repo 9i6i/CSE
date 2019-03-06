45f9aef62771e407a41b9fb5efc1121b1a794a6d
9i6i <44381250+9i6i@users.noreply.github.com>
2019-03-06 14:37:01 -0800

    jhgfdgkujguh

--- BrisaSerrano-Item.py
+++ BrisaSerrano-Item.py
@@ -1,2 +1,14 @@
 class Item(object):
-    
+    def __init__(self,name):
+        self.name = name
+
+
+class Phone(Item):
+    def __init__(self, name):
+        super(Phone, self).__init__(name)
+        self.power = True
+        self.batter_left = 100
+
+    def power(self):
+        self.power = True
+        print("")
