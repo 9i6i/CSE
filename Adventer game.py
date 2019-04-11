b38cb1fd54fe26c7b986806a3b6c42bdad4075bc
9i6i <44381250+9i6i@users.noreply.github.com>
2019-04-11 14:32:05 -0700

    hfidjiofhryosdnhfll;gfhugknfvnjr

--- Adventer game.py
+++ Adventer game.py
@@ -2,12 +2,12 @@ class Room(object):
     def __init__(self, description="INSERT DESCRIPTION HERE", name=None, north=None, south=None,
                  east=None, west=None, up=None, down=None, items=None):
         self.name = name
-        self.north = north
-        self.south = south
-        self.east = east
-        self.west = west
-        self.up = up
-        self.down = down
+        self.n = north
+        self.s = south
+        self.e = east
+        self.w = west
+        self.u = up
+        self.d = down
         self.description = description
         self.items = items
 
@@ -30,7 +30,7 @@ class Armor(Item):
 
 
 class Inventory(object):
-    def __init__(self, items=[]):
+    def __init__(self, items=()):
         self.items = items
 
 
@@ -89,6 +89,7 @@ class Player(object):
 
 monstercandy = MonsterCandy("Monster Candy")
 knife = Knife("Le knife")
+Phone = Phone("A Phone")
 
 R19A = Room("This is a room.", "Mr.Wiebe's room")
 parking_lot = Room("This is where you can park your car", "This is the parking lot")
@@ -98,13 +99,17 @@ The_Ruins = Room("You fell into the hole now you are in a room that has golden f
 Toriel_house = Room("as you were walking in the ruins you see a small house and the door was open so you walk"
                     "in", "Toriel's house")
 Snowdin = Room("There is snow in this room??", "You just past the ruins now you are in snowdin")
-Sans_and_Papy_house = Room("As you walked through Snowdin you spotted a house with two mailbox and both of th ")
+Sans_and_Papy_house = Room("As you walked through Snowdin you spotted a house with two"
+                           " mailbox and both of them have a name on both sans and "
+                           "papyrus", "you are in front of a monster house who have a lab in the back")
 Water_Fall = Room("There are blue flowers that repeat what you say", " You are now in Waterfall")
-True_lab = Room("You see ghost like monsters that creep you out", "This is true lab")
+Gaster_door = Room("there is a weird door in the middle of waterfall wall you stared at it for a good 20 second "
+                   "and it disappear", "Le gasters door")
 Hotland = Room("You look around and you see lava and you question how is this possible then "
                "stop caring and you move on to the next area", "You are in hotland")
 The_core = Room("There are wired everywhere you see you wonder"
                 " what this place use to be", "You are in the core now")
+True_lab = Room("You see ghost like monsters that creep you out", "This is true lab")
 New_home = Room("You look around there building everywhere some nice some doesn't"
                 " anyway you move on", "You are in new home")
 Judgement_hall = Room("There windows everywhere and it looked very nice", "Judgement hall")
@@ -115,31 +120,37 @@ The_Barrier = Room("You have reached the barrier this is where "
                    " years ago. you want to set them free?!?!?!?!?!", "The barrier where it all ends")
 
 R19A.items = monstercandy
-The_Ruins.items = knife
+parking_lot.items = Phone
+Mt_Abbot.items = knife
 
 
-R19A.north = parking_lot
-parking_lot.south = Mt_Abbot
-Mt_Abbot.up = The_Ruins
-The_Ruins.up = Snowdin
-Snowdin.west = Water_Fall
-Water_Fall.down = Hotland
-Hotland.east = The_core
-The_core.north = New_home
-New_home.east = Judgement_hall
-Judgement_hall.west = The_Barrier
-
+R19A.n = parking_lot
+parking_lot.s = Mt_Abbot
+Mt_Abbot.u = The_Ruins
+The_Ruins.u = Toriel_house
+Toriel_house.n = Snowdin
+Snowdin.w = Sans_and_Papy_house
+Sans_and_Papy_house.u = Water_Fall
+Water_Fall.u = Gaster_door
+Gaster_door.d = Water_Fall
+Water_Fall.d = Hotland
+Hotland.e = The_core
+The_core.n = New_home
+New_home.e = Judgement_hall
+Judgement_hall.w = Throne_room
+Throne_room.w = The_Barrier
+The_Barrier.e = Throne_room
 
 player = Player(R19A)
 
-directions = ['north', 'south', 'east', 'west', 'up', 'down']
+directions = ['n', 's', 'e', 'w', 'u', 'd']
 playing = True
 
 while playing:
     print(player.current_location.name)
     print(player.current_location.description)
     if player.current_location.items is not None:
-        print("There is  %s here" % player.current_location.items.name)
+        print("There is %s here" % player.current_location.items.name)
 
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
@@ -156,15 +167,13 @@ while playing:
         item_name = command[8:]
 
         found_item = None
-        for item in player.current_location.items:
-            if item_name == item_name:
-                found_item = item
+        if player.current_location.items.name == item_name:
+            found_item = player.current_location.items
 
         if found_item is None:
             print("i don't see one~~~")
         else:
             player.inventory.append(found_item)
-            player.current_location.items.remove(found_item)
             print("grab %s" % item_name)
     else:
         print("Command not recognized.")
