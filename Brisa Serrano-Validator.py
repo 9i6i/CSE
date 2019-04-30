f23512357c929f05489889661dd72aff781025cb
9i6i <44381250+9i6i@users.noreply.github.com>
2019-04-30 14:22:32 -0700

    jkfytdyghfr6ftydfrtsyuj

---
+++ Brisa Serrano-Validator.py
@@ -0,0 +1,17 @@
+test_num = "4699817949151360"
+
+
+def reverse_it(string):
+    return string[:15]
+
+
+print(reverse_it(test_num))
+
+
+def validate(num: str):
+    for index in range(len(num)):
+        int_version = int(num[index])
+
+
+def reverse_it(string):
+    return string[::-1]
