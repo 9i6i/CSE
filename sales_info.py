f7729d7ca755a2cfd33d1483d9ead8e296ef3edb
9i6i <44381250+9i6i@users.noreply.github.com>
2019-05-10 14:49:02 -0700

    hoi 2

--- sales_info.py
+++ sales_info.py
@@ -2,9 +2,11 @@ import csv
 
 with open("Sales Records.csv", "r") as csv_file_things:
     reader = csv.reader(csv_file_things)
+    total = 0
     for row in reader:
         profit = row[13]
         category = row[2]
-        print(profit)
         if category == "Fruits":
             print(profit, category)
+            total += float(profit)
+    print(total)
