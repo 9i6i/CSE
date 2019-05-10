eb67211a6138d0150b0fc7edace7f8987c5d45ed
9i6i <44381250+9i6i@users.noreply.github.com>
2019-05-10 14:48:39 -0700

    hoi 2

--- sales_info.py
+++ sales_info.py
@@ -2,7 +2,6 @@ import csv
 
 with open("Sales Records.csv", "r") as csv_file_things:
     reader = csv.reader(csv_file_things)
-
     for row in reader:
         profit = row[13]
         category = row[2]
