eaa4ebedc8679b4060ab63d98979ed0ec0a0b195
9i6i <44381250+9i6i@users.noreply.github.com>
2019-05-17 14:47:50 -0700

    lol :laughing:

--- Brisa Serrano-Sales_Info.py
+++ Brisa Serrano-Sales_Info.py
@@ -3,8 +3,6 @@ import csv
 with open("Sales Records.csv", "r") as csv_file_things:
     reader = csv.reader(csv_file_things)
     total = 0
-    fruit = 0
-    meat = 0
     for row in reader:
         profit = row[13]
         category = row[2]
